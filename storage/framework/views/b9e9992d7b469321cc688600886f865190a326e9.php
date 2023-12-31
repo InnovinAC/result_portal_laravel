5bafc3ac33276c6f870df2f1af044db64c68c2b4
Innovin Anuonye <102300150+InnovinAC@users.noreply.github.com>
2023-12-31 21:14:21 +0100

    Delete storage/framework/views directory

--- storage/framework/views/b9e9992d7b469321cc688600886f865190a326e9.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('title', 'Too many requests'); ?>
-<?php $__env->startSection('code', 429); ?>
-<?php $__env->startSection('text', 'You have submitted too many requests. Are you a bot? Please wait a bit'); ?>
-
-
-<?php echo $__env->make('errors.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\result_laravel\resources\views/errors/429.blade.php ENDPATH**/ ?>
